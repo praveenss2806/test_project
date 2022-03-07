427fb4ab83c1d7261a15949a7f6aab6b99285ed8
Praveen Sivakumar <61496527+praveenss2806@users.noreply.github.com>
2022-03-07 16:22:53 +0530

    Add files via upload

---
+++ KLA Hackathon/code.py
@@ -0,0 +1,22 @@
+import threading
+import time
+
+start=time.perf_counter()
+
+def do_something():
+    print('Sleeping 1 Second')
+    time.sleep(1)
+    print('Done Sleeping')
+
+t1=threading.Thread(target=do_something)
+t2=threading.Thread(target=do_something)
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+finish=time.perf_counter()
+
+print("Seconds:",finish-start)
